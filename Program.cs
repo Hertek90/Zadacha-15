3ea9d34b1c472d9990e38fac24eab7467079a1bf
«Apolon» <hertek1990@gmail.com>
2023-03-09 21:56:42 +0700

    Zadacha 13

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+System.Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 8)
+{
+    if (number > 5)
+    {
+        System.Console.WriteLine("Да");
+    }
+    else
+    {
+        System.Console.WriteLine("Нет");
+    }
+}
+else
+{
+    System.Console.WriteLine("Неверное число недели");
+}
